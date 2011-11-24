61f4d66298174b0a20f8a9536a96b6599033f05e
Simon Howard <fraggle@gmail.com>
2011-11-24 01:03:49 +0000

    Detect and support dragging in OS X.

--- pymouse/mac.py
+++ pymouse/mac.py
@@ -20,20 +20,33 @@ from base import PyMouseMeta, PyMouseEventMeta
 
 pressID = [None, kCGEventLeftMouseDown, kCGEventRightMouseDown, kCGEventOtherMouseDown]
 releaseID = [None, kCGEventLeftMouseUp, kCGEventRightMouseUp, kCGEventOtherMouseUp]
+dragID = [ None, kCGEventLeftMouseDragged, kCGEventRightMouseDragged, kCGEventOtherMouseDragged ]
 
 class PyMouse(PyMouseMeta):
+
+    def __init__(self, *args):
+        PyMouseMeta.__init__(self, *args)
+        self.buttondown = [ False, False, False, False ]
+
     def press(self, x, y, button = 1):
         event = CGEventCreateMouseEvent(None, pressID[button], (x, y), button - 1)
         CGEventPost(kCGHIDEventTap, event)
+        self.buttondown[button] = True
 
     def release(self, x, y, button = 1):
         event = CGEventCreateMouseEvent(None, releaseID[button], (x, y), button - 1)
         CGEventPost(kCGHIDEventTap, event)
+        self.buttondown[button] = False
 
     def move(self, x, y):
-        move = CGEventCreateMouseEvent(None, kCGEventMouseMoved, (x, y), 0)
+        if True in self.buttondown:
+            button = self.buttondown.index(True)
+            event_type = dragID[button]
+        else:
+            event_type = kCGEventMouseMoved
+
+        move = CGEventCreateMouseEvent(None, event_type, (x, y), 0)
         CGEventPost(kCGHIDEventTap, move)
-        
 
     def position(self):
         loc = NSEvent.mouseLocation()
